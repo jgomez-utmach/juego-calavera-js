e338f5dfaf866027f6b62c192e7a53d7d74e3783
jgomez-utmach <jgomez20@utmachala.edu.ec>
2023-12-30 20:17:17 -0500

    Canvas responsive

--- game.js
+++ game.js
@@ -1,36 +1,37 @@
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
-window.addEventListener('load', draw);
 
-function draw() {
-  //Variable que contendrá el tamaño del canvas.
-  let canvasSize;
+//Declaramos como variables globales el tamaño del canvas y el tamaño de los elementos
+let canvasSize;
+let elementsSize;
 
-  //condicional que busca que el canvas sea cuadrado, por lo que se evalúa si la altura es mayor que el ancho.
+window.addEventListener('load', setCanvasSize);
+//añadimos un listener para que cuando se redimensione la pantalla se redimensione el canvas
+window.addEventListener('resize', setCanvasSize);
+
+//Separamos la lógica del dibujo (draw) y la lógica que calcula el tamaño del canvas (setCanvasSize)
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
-    //Si la altura de la ventana es mayor que el ancho, el tamaño de canvasSize será el ancho de la ventana por 0.8.
     canvasSize = window.innerWidth * 0.8;
   } else {
-    //Caso contrario el tamaño de canvasSize será el alto de la ventana por 0.8.
     canvasSize = window.innerHeight * 0.8;
   }
-
-  //Se establece el tamaño del canvas.
+  
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
-
-  //Variable que contendrá el tamaño de los elementos del juego.
-  const elementsSize = canvasSize / 10;
-
-  console.log({ canvasSize, elementsSize });
   
-  //Definimos el tamaño de los elementos del juego.
+  elementsSize = canvasSize / 10;
+
+  draw();
+}
+
+function draw() {
+  console.log({ canvasSize, elementsSize });
+
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
-  //Repite el proceso que dibuja el emoji 10 veces.
   for (let i = 1; i <= 10; i++) {
-    //Dibujamos el emoji en la posición x = elementsSize, y = elementsSize * i.
-    game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
   }
 }
